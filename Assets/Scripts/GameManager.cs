e7804cb559b9d498ab88feff7c477aac35c68495
zacharydesoto <zacharysdesoto@gmail.com>
2024-03-03 15:19:53 -0800

    Final commit :interrobang:

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     int numEnemies;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         numEnemies = 0;
     }
