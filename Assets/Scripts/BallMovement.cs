752869822dbde261a38cb26400e82820c5cf9fa7
zacharydesoto <zacharysdesoto@gmail.com>
2024-03-03 12:19:36 -0800

    Implement Ball Physics :smirk_cat:

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    [SerializeField] float startPower;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        InitializeMovement();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void InitializeMovement()
+    {
+        float angle = Random.Range(5 * Mathf.PI / 4, 7 * Mathf.PI / 4);
+        Vector2 forceVector = new Vector2(startPower * Mathf.Cos(angle), startPower * Mathf.Sin(angle));
+        rb.AddForce(forceVector);
     }
 }
